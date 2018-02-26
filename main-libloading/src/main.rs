99de3dabb35a9800af36a8439cab3d03cc216459
Nguyễn Tuấn Anh <ubolonton@gmail.com>
2018-02-26 17:59:19 +0700

    Add some debugging prints

--- main-libloading/src/main.rs
+++ main-libloading/src/main.rs
@@ -1,6 +1,6 @@
+extern crate libc;
 extern crate libloading;
 
-use std::os::raw;
 use std::{thread, time};
 use libloading::{Library, Symbol};
 
@@ -9,29 +9,64 @@ const LIB_PATH: &'static str = "../module/target/debug/libmodule.dylib";
 
 extern "C" {
     fn _dyld_image_count() -> u32;
+
+    fn _dyld_register_func_for_add_image(
+        func: unsafe extern "C" fn(mh: *const libc::mach_header, vmaddr_slide: libc::intptr_t)
+    );
+    fn _dyld_register_func_for_remove_image(
+        func: unsafe extern "C" fn(mh: *const libc::mach_header, vmaddr_slide: libc::intptr_t)
+    );
+    fn _tlv_atexit(dtor: unsafe extern fn(*mut libc::c_void), arg: *mut libc::c_void);
 }
 
 fn image_count() -> u32 {
     unsafe { _dyld_image_count() }
 }
 
+unsafe extern fn on_thread_exit(obj_addr: *mut libc::c_void) {
+    println!("-- thread exiting {:?}", obj_addr);
+}
+
+unsafe extern "C" fn image_added(mh: *const libc::mach_header, vmaddr_slide: libc::intptr_t) {
+    println!("-- image-added  : {:?} {}", mh, vmaddr_slide);
+}
+
+unsafe extern "C" fn image_removed(mh: *const libc::mach_header, vmaddr_slide: libc::intptr_t) {
+    println!("-- image-removed: {:?} {}", mh, vmaddr_slide);
+}
+
+fn use_lib() {
+    // println!("use_lib in thread {:?}", thread::current().id());
+
+    unsafe {
+        _tlv_atexit(on_thread_exit, std::ptr::null_mut());
+    }
+
+    let lib = Library::new(LIB_PATH)
+        .expect("Library not found");
+
+    println!("image count after loading  : {}", image_count());
+
+    unsafe {
+        let init: Symbol<unsafe extern "C" fn()> =
+            lib.get(b"init").expect("Symbol not found");
+        init()
+    };
+
+    drop(lib);
+
+    println!("image count after unloading: {}", image_count());
+}
+
 fn main() {
+    unsafe {
+        _dyld_register_func_for_add_image(image_added);
+        _dyld_register_func_for_remove_image(image_removed);
+    }
+
+    // println!("main thread {:?}", thread::current().id());
     loop {
         thread::sleep(time::Duration::from_secs(1));
-
-        let lib = Library::new(LIB_PATH)
-            .expect("Library not found");
-
-        println!("image count after loading  : {}", image_count());
-
-        unsafe {
-            let init: Symbol<unsafe extern "C" fn()> =
-                lib.get(b"init").expect("Symbol not found");
-            init()
-        };
-
-        drop(lib);
-
-        println!("image count after unloading: {}", image_count());
+        thread::spawn(use_lib).join().unwrap();
     }
 }
--- main/src/main.rs
+++ main/src/main.rs
@@ -1,49 +1,82 @@
+extern crate libc;
+
 use std::ffi::{CString, CStr};
-use std::os::raw;
 use std::{thread, time};
 use std::mem;
 
-const RTLD_NOW: raw::c_int = 2;
+const LIB_PATH: &'static str = "../module/target/debug/libmodule.dylib";
+// const LIB_PATH: &'static str = "../module/target/libmodule-c.so";
+
+const RTLD_NOW: libc::c_int = 2;
 extern "C" {
     fn _dyld_image_count() -> u32;
-    fn dlopen(filename: *const raw::c_char, flags: raw::c_int) -> *mut raw::c_void;
-    fn dlclose(handle: *mut raw::c_void) -> raw::c_int;
-    fn dlsym(handle: *mut raw::c_void, symbol: *const raw::c_char) -> *mut raw::c_void;
-    fn dlerror() -> *mut raw::c_char;
+    fn dlopen(filename: *const libc::c_char, flags: libc::c_int) -> *mut libc::c_void;
+    fn dlclose(handle: *mut libc::c_void) -> libc::c_int;
+    fn dlsym(handle: *mut libc::c_void, symbol: *const libc::c_char) -> *mut libc::c_void;
+    fn dlerror() -> *mut libc::c_char;
+
+    fn _dyld_register_func_for_add_image(
+        func: unsafe extern "C" fn(mh: *const libc::mach_header, vmaddr_slide: libc::intptr_t)
+    );
+    fn _dyld_register_func_for_remove_image(
+        func: unsafe extern "C" fn(mh: *const libc::mach_header, vmaddr_slide: libc::intptr_t)
+    );
+    fn _tlv_atexit(dtor: unsafe extern fn(*mut libc::c_void), arg: *mut libc::c_void);
 }
 
 fn image_count() -> u32 {
     unsafe { _dyld_image_count() }
 }
 
-const LIB_PATH: &'static str = "../module/target/debug/libmodule.dylib";
-// const LIB_PATH: &'static str = "../module/target/libmodule-c.so";
+unsafe extern fn on_thread_exit(obj_addr: *mut libc::c_void) {
+    println!("-- thread exiting {:?}", obj_addr);
+}
+
+unsafe extern "C" fn image_added(mh: *const libc::mach_header, vmaddr_slide: libc::intptr_t) {
+    println!("-- image-added  : {:?} {}", mh, vmaddr_slide);
+}
+
+unsafe extern "C" fn image_removed(mh: *const libc::mach_header, vmaddr_slide: libc::intptr_t) {
+    println!("-- image-removed: {:?} {}", mh, vmaddr_slide);
+}
+
+fn use_lib() {
+    println!("use_lib in thread {:?}", thread::current().id());
+
+    unsafe {
+        let handle = dlopen(CString::new(LIB_PATH).unwrap().into_raw(), RTLD_NOW);
+
+        _tlv_atexit(on_thread_exit, std::ptr::null_mut());
+
+        println!("image count after loading  : {}", image_count());
+
+        let symbol = b"init";
+        let raw_init = dlsym(handle, CString::new(&symbol[..]).unwrap().into_raw());
+        if raw_init.is_null() {
+            println!("Failed to retrieve init symbol: {}", CStr::from_ptr(dlerror()).to_string_lossy());
+            return;
+        }
+
+        let init = mem::transmute::<*mut libc::c_void, unsafe extern fn()>(raw_init);
+        init();
+
+        if dlclose(handle) != 0 {
+            println!("Failed to close handle: {}", CString::from_raw(dlerror()).into_string().expect("Failed to retrieve dlerror"));
+            return;
+        }
+
+        println!("image count after unloading: {}", image_count());
+    }
+}
 
 fn main() {
+    unsafe {
+        _dyld_register_func_for_add_image(image_added);
+        _dyld_register_func_for_remove_image(image_removed);
+    }
+
     loop {
         thread::sleep(time::Duration::from_secs(1));
-
-        unsafe {
-            let handle = dlopen(CString::new(LIB_PATH).unwrap().into_raw(), RTLD_NOW);
-
-            println!("image count after loading  : {}", image_count());
-
-            let symbol = b"init";
-            let raw_init = dlsym(handle, CString::new(&symbol[..]).unwrap().into_raw());
-            if raw_init.is_null() {
-                println!("Failed to retrieve init symbol: {}", CStr::from_ptr(dlerror()).to_string_lossy());
-                return;
-            }
-
-            let init = mem::transmute::<*mut raw::c_void, unsafe extern fn()>(raw_init);
-            init();
-
-            if dlclose(handle) != 0 {
-                println!("Failed to close handle: {}", CString::from_raw(dlerror()).into_string().expect("Failed to retrieve dlerror"));
-                return;
-            }
-
-            println!("image count after unloading: {}", image_count());
-        }
+        thread::spawn(use_lib).join().unwrap();
     }
 }
