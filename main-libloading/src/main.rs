8eb919ef7dbe5429d405dc5a28387a6c1117c396
Nguyễn Tuấn Anh <ubolonton@gmail.com>
2018-02-26 11:23:51 +0700

    Separate 2 Rust approaches: libloading and raw dyld

---
+++ main-libloading/src/main.rs
@@ -0,0 +1,37 @@
+extern crate libloading;
+
+use std::os::raw;
+use std::{thread, time};
+use libloading::{Library, Symbol};
+
+// const LIB_PATH: &'static str = "../module/target/debug/libmodule.dylib";
+const LIB_PATH: &'static str = "../module-c/libmodule.so";
+
+extern "C" {
+    fn _dyld_image_count() -> u32;
+}
+
+fn image_count() -> u32 {
+    unsafe { _dyld_image_count() }
+}
+
+fn main() {
+    loop {
+        thread::sleep(time::Duration::from_secs(1));
+
+        let lib = Library::new(LIB_PATH)
+            .expect("Library not found");
+
+        println!("image count after loading  : {}", image_count());
+
+        unsafe {
+            let init: Symbol<unsafe extern "C" fn()> =
+                lib.get(b"init").expect("Symbol not found");
+            init()
+        };
+
+        drop(lib);
+
+        println!("image count after unloading: {}", image_count());
+    }
+}
--- main/src/main.rs
+++ main/src/main.rs
@@ -1,37 +1,49 @@
-extern crate libloading;
-
+use std::ffi::{CString, CStr};
 use std::os::raw;
 use std::{thread, time};
-use libloading::{Library, Symbol};
+use std::mem;
 
-// const LIB_PATH: &'static str = "../module/target/debug/libmodule.dylib";
-const LIB_PATH: &'static str = "../module-c/libmodule.so";
-
+const RTLD_NOW: raw::c_int = 2;
 extern "C" {
     fn _dyld_image_count() -> u32;
+    fn dlopen(filename: *const raw::c_char, flags: raw::c_int) -> *mut raw::c_void;
+    fn dlclose(handle: *mut raw::c_void) -> raw::c_int;
+    fn dlsym(handle: *mut raw::c_void, symbol: *const raw::c_char) -> *mut raw::c_void;
+    fn dlerror() -> *mut raw::c_char;
 }
 
 fn image_count() -> u32 {
     unsafe { _dyld_image_count() }
 }
 
+const LIB_PATH: &'static str = "../module-c/libmodule.so";
+// const LIB_PATH: &'static str = "../module/target/debug/libmodule.dylib";
+
 fn main() {
     loop {
         thread::sleep(time::Duration::from_secs(1));
 
-        let lib = Library::new(LIB_PATH)
-            .expect("Library not found");
-
-        println!("image count after loading  : {}", image_count());
-
         unsafe {
-            let init: Symbol<unsafe extern "C" fn()> =
-                lib.get(b"init").expect("Symbol not found");
-            init()
-        };
+            let handle = dlopen(CString::new(LIB_PATH).unwrap().into_raw(), RTLD_NOW);
 
-        drop(lib);
+            println!("image count after loading  : {}", image_count());
 
-        println!("image count after unloading: {}", image_count());
+            let symbol = b"init";
+            let raw_init = dlsym(handle, CString::new(&symbol[..]).unwrap().into_raw());
+            if raw_init.is_null() {
+                println!("Failed to retrieve get_message symbol: {}", CStr::from_ptr(dlerror()).to_string_lossy());
+                return;
+            }
+
+            let init = mem::transmute::<*mut raw::c_void, unsafe extern fn()>(raw_init);
+            init();
+
+            if dlclose(handle) != 0 {
+                println!("Failed to close handle: {}", CString::from_raw(dlerror()).into_string().expect("Failed to retrieve dlerror"));
+                return;
+            }
+
+            println!("image count after unloading: {}", image_count());
+        }
     }
 }
